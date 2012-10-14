01d1c39f0a00b8e7daf569fbbe860287db9e6cc1
Jon Miller <jonandrewmiller@gmail.com>
2012-10-14 17:09:55 +0000

    Deployment updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,16 @@
-require "bundler/capistrano"
 require "rvm/capistrano"
+require "bundler/capistrano"
 
-set :rvm_ruby_string, '1.9.3@tagr-server'
+set :rvm_ruby_string, "1.9.3@tagr-server"
 
-set :application, 'tagr-server'
-set :repository,  'https://github.com/jonmiller/tagr-server.git'
+set :application, "tagr-server"
+set :repository,  "https://github.com/jonmiller/tagr-server.git"
 
 set :scm, :git
 
-role :web, 'tetyl.com'
-role :app, 'tetyl.com'
-role :db,  'tetyl.com', :primary => true
+role :web, "tetyl.com"
+role :app, "tetyl.com"
+role :db,  "tetyl.com", :primary => true
 
 set :use_sudo, false
 set :keep_releases, 5
@@ -20,10 +20,11 @@ set :deploy_to, "/home/#{user}/apps/#{application}"
 
 ssh_options[:port] = 24242
 
-before 'deploy:setup', 'rvm:install_rvm'
-before 'deploy:setup', 'rvm:install_ruby'
+before "deploy:setup", "rvm:install_rvm"
+before "deploy:setup", "rvm:install_ruby"
 
-after 'deploy', 'deploy:cleanup'
+after "deploy", "deploy:cleanup"
+after "deploy", "rvm:trust_rvmrc"
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
@@ -31,5 +32,11 @@ namespace :deploy do
   task :stop do ; end
   task :restart, roles: :app, except: { no_release: true } do
     run "touch #{deploy_to}/current/tmp/restart.txt"
+  end
+end
+
+namespace :rvm do
+  task :trust_rvmrc do
+    run "rvm rvmrc trust #{release_path}"
   end
 end
